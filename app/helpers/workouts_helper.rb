24fd89c4c8b362abec31bc92862fe4939beb3c26
brittmmendez <brittmmendez@gmail.com>
2018-04-16 11:00:48 -0400

    finsihed index filter system

--- app/helpers/workouts_helper.rb
+++ app/helpers/workouts_helper.rb
@@ -13,11 +13,79 @@ module WorkoutsHelper
         @workouts = @user.workouts
       end
 
-    elsif !params[:category].blank? && !params[:training_type].blank? && !params[:difficulty].blank?
+#all 5 filters
+    elsif !params[:category].blank? && !params[:training_type].blank? && !params[:difficulty].blank? && !params[:min].blank? && !params[:max].blank?
       category =Workout.by_category(params[:category])
       training =category.by_training_type(params[:training_type])
-      @workouts =training.by_difficulty(params[:difficulty])
-  
+      difficulty =training.by_difficulty(params[:difficulty])
+      min = difficulty.by_min(params[:min])
+      @workouts =min.by_max(params[:max])
+
+#4 filters
+
+    elsif !params[:category].blank? && !params[:training_type].blank? && !params[:difficulty].blank? && !params[:min].blank?
+      category =Workout.by_category(params[:category])
+      training =category.by_training_type(params[:training_type])
+      difficulty =training.by_difficulty(params[:difficulty])
+      @workouts = difficulty.by_min(params[:min])
+      
+    elsif !params[:category].blank? && !params[:training_type].blank? && !params[:difficulty].blank? && !params[:max].blank?
+      category =Workout.by_category(params[:category])
+      training =category.by_training_type(params[:training_type])
+      difficulty =training.by_difficulty(params[:difficulty])
+      @workouts =difficulty.by_max(params[:max])
+
+    elsif !params[:category].blank? && !params[:training_type].blank? && !params[:min].blank? && !params[:max].blank?
+      category =Workout.by_category(params[:category])
+      training =category.by_training_type(params[:training_type])
+      min = training.by_min(params[:min])
+      @workouts =min.by_max(params[:max])
+
+    elsif !params[:category].blank? && !params[:difficulty].blank? && !params[:min].blank? && !params[:max].blank?
+      category =Workout.by_category(params[:category])
+      difficulty =category.by_difficulty(params[:difficulty])
+      min = difficulty.by_min(params[:min])
+      @workouts =min.by_max(params[:max])
+
+    elsif !params[:training_type].blank? && !params[:difficulty].blank? && !params[:min].blank? && !params[:max].blank?
+      training =Workout.by_training_type(params[:training_type])
+      difficulty =training.by_difficulty(params[:difficulty])
+      min = difficulty.by_min(params[:min])
+      @workouts =min.by_max(params[:max])
+
+#3 filters      
+   elsif !params[:category].blank? && !params[:training_type].blank? && !params[:difficulty].blank?
+     category =Workout.by_category(params[:category])
+     training =category.by_training_type(params[:training_type])
+     @workouts =training.by_difficulty(params[:difficulty])
+
+    # Cat Type Min 
+   elsif !params[:category].blank? && !params[:training_type].blank? && !params[:min].blank? 
+     category =Workout.by_category(params[:category])
+     training =category.by_training_type(params[:training_type])
+     @workouts = training.by_min(params[:min])
+      
+   elsif !params[:category].blank? && !params[:training_type].blank? && !params[:max].blank?
+     category =Workout.by_category(params[:category])
+     training =category.by_training_type(params[:training_type])
+     @workouts =trainingmin.by_max(params[:max])
+
+   elsif !params[:category].blank? && !params[:difficulty].blank? && !params[:min].blank? 
+     category =Workout.by_category(params[:category])
+     difficulty =category.by_difficulty(params[:difficulty])
+     @workouts = difficulty.by_min(params[:min])
+
+   elsif !params[:category].blank? && !params[:difficulty].blank? && !params[:max].blank?
+     category =Workout.by_category(params[:category])
+     difficulty =category.by_difficulty(params[:difficulty])
+     @workouts =difficulty.by_max(params[:max])
+
+   elsif !params[:category].blank? && !params[:min].blank? && !params[:max].blank?
+     category =Workout.by_category(params[:category])
+     min = category.by_min(params[:min])
+     @workouts =min.by_max(params[:max])
+
+#2 filters
     elsif !params[:category].blank? && !params[:training_type].blank?
       category =Workout.by_category(params[:category])
       @workouts =category.by_training_type(params[:training_type])
@@ -29,7 +97,36 @@ module WorkoutsHelper
     elsif !params[:training_type].blank? && !params[:difficulty].blank?
       training =Workout.by_training_type(params[:training_type])
       @workouts =training.by_difficulty(params[:difficulty])
-  
+
+    elsif !params[:category].blank? && !params[:min].blank?
+      category =Workout.by_category(params[:category])
+      @workouts = category.by_min(params[:min])
+      
+    elsif !params[:category].blank? && !params[:max].blank?
+      category =Workout.by_category(params[:category])
+      @workouts =category.by_max(params[:max])
+    
+    elsif !params[:training_type].blank? && !params[:min].blank?
+      training =Workout.by_training_type(params[:training_type])
+      @workouts = training.by_min(params[:min])
+     
+    elsif !params[:training_type].blank? && !params[:max].blank?
+      training =Workout.by_training_type(params[:training_type])
+      @workouts =training.by_max(params[:max])
+     
+    elsif !params[:difficulty].blank? && !params[:min].blank?
+      difficulty =Workout.by_difficulty(params[:difficulty])
+      @workouts = difficulty.by_min(params[:min])
+      
+    elsif !params[:difficulty].blank? && !params[:max].blank?
+      difficulty =Workout.by_difficulty(params[:difficulty])
+      @workouts =difficulty.by_max(params[:max])
+      
+    elsif !params[:min].blank? && !params[:max].blank?
+      min = Workout.by_min(params[:min])
+      @workouts =min.by_max(params[:max])
+
+#1 filters  
     elsif !params[:category].blank?
       @workouts =Workout.by_category(params[:category])
   
