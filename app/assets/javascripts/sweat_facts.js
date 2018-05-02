b74ae4d9e083cb8c7bdeedbec5d8344ddc36782a
Brittany Morris <brittmmendez@gmail.com>
2018-05-02 01:44:05 +0000

    added comment notes

--- app/assets/javascripts/sweat_facts.js
+++ app/assets/javascripts/sweat_facts.js
@@ -1,9 +1,10 @@
 $(function () {
+  // use Jquery event handler for whn button is clicked
   $(".js-more").on('click', function() {
-    // get the id from the data attribute
+    // use Jquery AJAX .get Method to get the id from the data attribute
     var id = $(this).data("id");
     $.get("/workouts/" + id + ".json", function(data) {
-       // Replace text of body-id div
+       // Replace text of body-id div with description
     $("#body-" + id).text(data["description"]);
     });
   });
